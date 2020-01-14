710254ffdf5e8e1e12d2548accdbc8c3f93ed83a
igorgo-hyuna <igor.gologots@hyuna.bb>
2020-01-14 18:41:11 +0200

    Added first commit

---
+++ app/js/common.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    /* Burger */
+    $(".toggle_mnu").click(function() {
+        $(".sandwich").toggleClass("active");
+    });
+    /* Burger. Hover */
+    $(".top_mnu ul a").click(function() {
+        $(".top_mnu").fadeOut(600);
+        $(".sandwich").toggleClass("active");
+        $(".top_text").css("opacity", "1");
+    }).append("<span>");
+
+    /* Burger. Open and close */
+    $(".toggle_mnu").click(function() {
+        if ($(".top_mnu").is(":visible")) {
+            $(".top_text").css("opacity", "1");
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeInUp animated");
+        } else {
+            $(".top_text").css("opacity", ".1");
+            $(".top_mnu").fadeIn(600);
+            $(".top_mnu li a").addClass("fadeInUp animated");
+        };
+    });
+
+    /* Parallax 
+    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
+
+    /* Resize
+    function heightDetect(){
+        $(".parallax-window").css("height", $(window).height());    
+    }
+    heightDetect();
+    $(window).resize(function(){
+        heightDetect();
+    });
+     */
+
+    /* Scrool animation
+    $(window).scroll(function() {
+
+        $('.mov').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 600) {
+                $(this).addClass('fadeIn animated');
+                $(this).css('opacity', '1');
+            }
+        });
+
+        $('.mov-left').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 600) {
+                $(this).addClass('fadeInLeft animated');
+                $(this).css('opacity', '1');
+            }
+        });
+
+        $('.mov-right').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 600) {
+                $(this).addClass('fadeInRight animated');
+                $(this).css('opacity', '1');
+            }
+        });
+    });*/
+});
