ba39493787bab95ef377474775a04e6df841910b
igorgo-hyuna <igor.gologots@hyuna.bb>
2020-01-21 18:18:35 +0200

    Fixed errors

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     /* Burger */
     $(".toggle_mnu").click(function() {
         $(".sandwich").toggleClass("active");
@@ -32,73 +33,34 @@ $(document).ready(function() {
 
     /* Timer */
     var today = new Date();
-            var tomorrow = new Date(today.getTime() + (72 * 60 * 60 * 1000));
-            var dayTomorrow = tomorrow.getDate();
-            var monthTomorrow = tomorrow.getMonth();
-            var yearTomorrow = tomorrow.getFullYear();
-            var arr=[
-            'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                    'July',
-                    'August',
-                    'September',
-                    'November',
-                    'December',
-            ];
-            var finalMounth = arr[monthTomorrow];
+    var tomorrow = new Date(today.getTime() + (1440 * 60 * 60 * 1000));
+    var dayTomorrow = tomorrow.getDate();
+    var monthTomorrow = tomorrow.getMonth();
+    var yearTomorrow = tomorrow.getFullYear();
+    var arr=[
+    'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'November',
+            'December',
+    ];
+    var finalMounth = arr[monthTomorrow];
 
-            var now = new Date();
-            $('.demo1').dsCountDown({
-                endDate: new Date(finalMounth + " " + dayTomorrow + ", " + yearTomorrow + " 12:57:00")
-            });
-
-            $(".ds-element.ds-element-seconds").css("display", "none");
-
-    /* Parallax 
-    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
-
-    /* Resize
-    function heightDetect(){
-        $(".parallax-window").css("height", $(window).height());    
-    }
-    heightDetect();
-    $(window).resize(function(){
-        heightDetect();
+    var now = new Date();
+    $('.demo1').dsCountDown({
+        endDate: new Date(finalMounth + " " + dayTomorrow + ", " + yearTomorrow + " 12:57:00")
     });
-    */
 
-    /* Scrool animation
-    $(window).scroll(function() {
+    // $(".ds-element.ds-element-seconds").css("display", "none");
 
-        $('.mov').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeIn animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-left').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInLeft animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-right').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInRight animated');
-                $(this).css('opacity', '1');
-            }
-        });
-    });*/
+    /* Form */
+    $(".wpcf7#wpcf7-f27-o1 .wpcf7-form p .wpcf7-submit").click(function() {
+        alert(1);
+    });
 });
